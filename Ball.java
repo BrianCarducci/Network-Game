2393e4e872ab68fd1e426e31c959f0ec3d9a053a
Rob Kmetz <robbeh.kmetz@aol.com>
2018-04-20 18:34:03 -0400

    Hopefully this doesn't ruin literally everything

--- Ball.java
+++ Ball.java
@@ -1,9 +1,15 @@
 import java.awt.geom.*;
+import java.math.*;
 
-public class Ball extends Ellipse2D.Double {
+public class Ball extends Ellipse2D.Double {	
+	
+    final private int width = 0;
+    final private int height = 0;
     private int xLoc = 0;
     private int yLoc = 0;
-    private int direction = 0;
+    private double xVel = 0;
+    private double yVel = 0;
+    private double angle;
 
     public Ball() {
     	
@@ -18,27 +24,47 @@ public class Ball extends Ellipse2D.Double {
 	@Override
 	public double getHeight() {
 		// TODO Auto-generated method stub
-		return 0;
+		return height;
 	}
 
 	@Override
 	public double getWidth() {
 		// TODO Auto-generated method stub
-		return 0;
+		return width;
 	}
 
 	@Override
 	public double getX() {
 		// TODO Auto-generated method stub
-		return 0;
+		return xLoc;
 	}
 
 	@Override
 	public double getY() {
 		// TODO Auto-generated method stub
-		return 0;
+		return yLoc;
 	}
-
+	
+	public double getXVel() {
+		return xVel;
+	}
+	
+	public double getYVel() {
+		return yVel;
+	}
+	
+	public void setLoc(int newX, int newY) {
+		xLoc = newX;
+		yLoc = newY;
+	}
+	
+	public void setVel(double newxVel, double newyVel) {
+		xVel = newxVel;
+		yVel = newyVel;
+	}
+	
+	
+	
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
